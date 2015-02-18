249b8d318c2fe7d6c20bbbd856f61c2ab9afd7f2
SanchezSobrino <sanchezsobrino@gmail.com>
2015-02-18 14:16:24 +0100

    El motor de scripts ahora soporta argumentos encerrados entre comillas (") como un único argumento (Ej. "hola que tal" sería un único argumento).

--- src/DrawTextPanelSF.cpp
+++ src/DrawTextPanelSF.cpp
@@ -14,11 +14,20 @@ namespace argosServer {
     for(int i = 0; i < 3; ++i)
       size += com.addFloat(getArgAsFloat(args[i]));
 
+    // Font size
+    size += com.addInt(getArgAsInt(args[3]));
+
     // Text
-    size += com.addChars(args[3].c_str(), 32);
+    size += com.addChars(args[4].c_str(), 32);
+
+    std::cout << args[4].c_str() << std::endl;
 
     // Position
-    for(int i = 4; i < 7; ++i)
+    for(int i = 5; i < 8; ++i)
+      size += com.addFloat(getArgAsFloat(args[i]));
+
+    // Size
+    for(int i = 8; i < 10; ++i)
       size += com.addFloat(getArgAsFloat(args[i]));
 
     _properties["bytes"] = std::to_string(size);
